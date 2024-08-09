09db0138228c5fa89b605ab05509b76f740f3b0b
Daniel Coutinho <daniel.mar.coutinho@gmail.com>
2024-08-08 23:46:09 -0400

    regression example

--- autograding_code_and_data/setup.sh
+++ autograding_code_and_data/setup.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
+apt-get update -qq
+apt-get install --no-install-recommends software-properties-common dirmngr
+wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+
 # or you can do it the slow way...
 apt-get install -y libxml2-dev libcurl4-openssl-dev libssl-dev
-apt-get install -y r-base
+apt install -y r-base
+apt-get install -y cmake
 
 # these lines install the packages that is needed both
 # 1. the student code 
 # 2. the autograding code
 Rscript -e "install.packages('gradeR')" 
 Rscript -e "install.packages('stringr')" 
+##Rscript -e "install.packages('Matrix')"
+Rscript -e "install.packages('AER')"
