424f974d5a9bc6e091deddd4ff3bddbf5251addb
Daniel Coutinho <36049697+danmrc@users.noreply.github.com>
2024-08-29 23:49:11 -0400

    Update setup.sh

--- autograding_code_and_data/setup.sh
+++ autograding_code_and_data/setup.sh
@@ -12,5 +12,6 @@ apt install -y r-base
 # these lines install the packages that is needed both
 # 1. the student code 
 # 2. the autograding code
+Rscript -e "install.packages('AER')" 
 Rscript -e "install.packages('gradeR')" 
 Rscript -e "install.packages('stringr')" 
