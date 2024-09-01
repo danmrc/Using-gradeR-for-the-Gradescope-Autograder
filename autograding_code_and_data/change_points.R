0a3bb2a334b89b941aef9e1081d3ba4deb9b2a58
Daniel Coutinho <daniel.mar.coutinho@gmail.com>
2024-09-01 16:55:12 -0400

    hopefully changes points

---
+++ autograding_code_and_data/change_points.R
@@ -0,0 +1,11 @@
+library(jsonlite)
+library(purrr)
+
+new_max_score <- 3
+
+original <- read_json("/autograder/results/results.json",simplifyVector = TRUE)
+original$tests$max_score <- new_max_score
+original$tests$score <- new_max_score*original$tests$score
+original$score <- sum(original$tests$score)
+
+write_json(original,"/autograder/results/results.json",dataframe = "rows")
