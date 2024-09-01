ae4f11d7cc71a8d2f693d29e28fb412835e00680
Daniel Coutinho <daniel.mar.coutinho@gmail.com>
2024-09-01 17:18:15 -0400

    Update change_points.R

--- autograding_code_and_data/change_points.R
+++ autograding_code_and_data/change_points.R
@@ -8,4 +8,4 @@ original$tests$max_score <- new_max_score
 original$tests$score <- new_max_score*original$tests$score
 original$score <- sum(original$tests$score)
 
-write_json(original,"/autograder/results/results.json",dataframe = "rows")
+write_json(original,"/autograder/results/results2.json",dataframe = "rows", auto_unbox = TRUE)
